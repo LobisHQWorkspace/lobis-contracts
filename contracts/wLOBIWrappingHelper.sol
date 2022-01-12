c63e61f8a8a31e505b7c0ef9c14fbe4cf3335ee4
maegner <francisco.aguiar@sky.uk>
2022-01-12 18:15:52 +0000

    Use SafeERC20

--- contracts/wLOBIWrappingHelper.sol
+++ contracts/wLOBIWrappingHelper.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.7.5;
 
 import "./libraries/SafeMath.sol";
 import "./libraries/Address.sol";
+import "./libraries/SafeERC20.sol";
 
 import "./interfaces/IERC20.sol";
 import "./interfaces/IwLOBI.sol";
@@ -13,6 +14,8 @@ contract wLOBIWrappingHelper {
 
     using Address for address;
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    using SafeERC20 for IwLOBI;
 
     /* ========== MODIFIERS ========== */
 
@@ -43,7 +46,7 @@ contract wLOBIWrappingHelper {
      * @return wBalance_ uint
      */
     function wrap(address _to, uint256 _amount) external returns (uint256 wBalance_) {
-        _safeTransferFrom(sLOBI, msg.sender, address(this), _amount);
+        sLOBI.safeTransferFrom(msg.sender, address(this), _amount);
         wBalance_ = wLOBI.balanceTo(_amount);
         wLOBI.mint(_to, wBalance_);
     }
@@ -57,7 +60,7 @@ contract wLOBIWrappingHelper {
     function unwrap(address _to, uint256 _amount) external returns (uint256 sBalance_) {
         wLOBI.burn(msg.sender, _amount);
         sBalance_ = wLOBI.balanceFrom(_amount);
-        _safeTransfer(sLOBI, _to, sBalance_);
+        sLOBI.safeTransfer(_to, sBalance_);
     }
 
     /* ========== ADMIN FUNCTIONS ========== */
@@ -67,42 +70,4 @@ contract wLOBIWrappingHelper {
         require(_wLOBI != address(0), "Zero address: wLOBI");
         wLOBI = IwLOBI(_wLOBI);
     }
-
-    /* ========== INTERNAL FUNCTIONS ========== */
-
-    /** 
-     *  @notice Transfers tokens to the given destination
-     *  @notice Errors with 'TRANSFER_FROM_FAILED' if transfer fails
-     *  @notice Safe IERC20 and ETH transfer library that safely handles missing return values.
-     *  @notice Modified from Uniswap (https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/libraries/TransferHelper.sol) (Taken from Olympus)
-     *  @param token The Contract of the token that is being transfered
-     *  @param to The destination address of the transfer
-     *  @param amount The amount to be transferred
-     */
-    function _safeTransfer(IERC20 token, address to, uint256 amount) internal {
-        (bool success, bytes memory data) = address(token).call(
-            abi.encodeWithSelector(IERC20.transfer.selector, to, amount)
-        );
-
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FAILED");
-    }
-
-    /** 
-     *  @notice Transfers tokens from the targeted address to the given destination
-     *  @notice Errors with 'TRANSFER_FROM_FAILED' if transfer fails
-     *  @notice Safe IERC20 and ETH transfer library that safely handles missing return values.
-     *  @notice Modified from Uniswap (https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/libraries/TransferHelper.sol) (Taken from Olympus)
-     *  @param from The source adress of the transfer
-     *  @param token The Contract of the token that is being transfered
-     *  @param to The destination address of the transfer
-     *  @param amount The amount to be transferred
-     */
-    function _safeTransferFrom(IERC20 token, address from, address to, uint256 amount) internal {
-        (bool success, bytes memory data) = address(token).call(
-            abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, amount)
-        );
-
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FROM_FAILED");
-    }
-
 }
